50aca41ff2180cb9665180a0470080cc7ec2d481
Kaushik Mishra <kaushik530411@gmail.com>
2018-11-14 20:56:19 -0500

    Added the 'main' and code to take input stream.

--- project2.s
+++ project2.s
@@ -5,6 +5,12 @@
 	input_long: .asciiz "Input is too long."
 
 .text
+main:
+	li $v0, 8  #  Taking input stream
+	la $a0, buffer  #  load byte space into address
+	li $a1, 10000  #  allot the byte space for string
+	syscall
+
 exit:
 	li $v0, 10 # end the program
 	syscall
