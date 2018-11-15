db0e18fb0fe12a1569e154eaebd5566592a286ef
Kaushik Mishra <kaushik530411@gmail.com>
2018-11-14 20:57:46 -0500

    Moved the input stream to two different temperory registers for later use.

--- project2.s
+++ project2.s
@@ -11,6 +11,9 @@ main:
 	li $a1, 10000  #  allot the byte space for string
 	syscall
 
+	move $t0, $a0  #  move string to $t0
+	move $t7, $a0  #  a copy of string in other register for future use
+
 exit:
 	li $v0, 10 # end the program
 	syscall
