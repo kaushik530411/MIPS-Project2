db9560c5996f0795419564c806f2a987038c32de
Kaushik Mishra <kaushik530411@gmail.com>
2018-11-14 20:53:51 -0500

    Added the '.text' section and code to exit the program.

--- project2.s
+++ project2.s
@@ -3,3 +3,8 @@
 	input_empty: .asciiz "Input is empty."
 	input_invalid: .asciiz "Invalid base-35 number."
 	input_long: .asciiz "Input is too long."
+
+.text
+exit:
+	li $v0, 10 # end the program
+	syscall
